c7f225343ce5fc4c62ac31e5fdf8a1e3877300c5
Leonardo Garcia Gavilan <leonardogarciagavilan@MacBook-Air-de-Leonardo.local>
2026-04-10 22:58:53 -0400

    conexion a backend en produccion

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getAccessToken, subscribeToAccessToken, setAccessTokenGlobal } from '../utils/token';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_URL = import.meta.env.VITE_API_URL || 'https://presta-tech-api.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
